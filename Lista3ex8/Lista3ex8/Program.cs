8c8838b4b46a5a211dee6e57560f0ce703a1c9a3
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-05-24 15:19:59 -0300

    Add files via upload

---
+++ Lista3ex8/Lista3ex8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int soma = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                soma += i;
+            }
+
+            Console.WriteLine("A soma dos números de 1 a 100 é: {0} ", soma);
+        }
+    }
+}
