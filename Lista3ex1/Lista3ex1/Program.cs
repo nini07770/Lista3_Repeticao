ef32527540a5f5bea72881d9a3419af2cf1880a7
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-05-24 13:30:34 -0300

    Add files via upload

---
+++ Lista3ex1/Lista3ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+
+            Console.Write("Digite um valor positivo: ");
+            valor = double.Parse(Console.ReadLine());
+
+            while (valor <= 0)
+            {
+                Console.Write("Valor inválido. Digite um valor positivo: ");
+                valor = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("O valor digitado foi: {0} ", valor );
+        }       
+          
+    }
+}
