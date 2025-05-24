dbf35a29cced35449e5a8e4005fe0639620a3f90
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-05-24 14:01:32 -0300

    Add files via upload

---
+++ Lista3ex4/Lista3ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i <= 10; i += 1)
+            {
+                Console.WriteLine("5 x {0} = {1}", i, 5 * i);
+            }
+        }
+    }
+}
