57353448464d6c4e3bafbc1ae8d7895d00dcea7f
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-05-24 15:31:20 -0300

    Add files via upload

---
+++ Lista3ex9/Lista3ex9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 1, n2 = 1, n3;
+
+            Console.WriteLine("Os primeiros 30 números da série de Fibonacci: ");
+
+            for (int i = 1; i <= 30; i++)
+            {
+                Console.WriteLine(n1);
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.WriteLine("FIM");
+        }
+    }
+}
