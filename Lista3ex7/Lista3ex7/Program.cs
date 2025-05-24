759e15259c83650b24e26e2e35e7a7cc54d38e51
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-05-24 15:10:18 -0300

    Add files via upload

---
+++ Lista3ex7/Lista3ex7/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("1 x {0} = {1}", i, 1 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("2 x {0} = {1}", i, 2 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("3 x {0} = {1}", i, 3 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("4 x {0} = {1}", i, 4 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("5 x {0} = {1}", i, 5 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("6 x {0} = {1}", i, 6 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("7 x {0} = {1}", i, 7 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("8 x {0} = {1}", i, 8 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("9 x {0} = {1}", i, 9 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("10 x {0} = {1}", i, 10 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("11 x {0} = {1}", i, 11 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("12 x {0} = {1}", i, 12 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("13 x {0} = {1}", i, 13 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("14 x {0} = {1}", i, 14 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("15 x {0} = {1}", i, 15 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("16 x {0} = {1}", i, 16 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("17 x {0} = {1}", i, 17 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("18 x {0} = {1}", i, 18 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("19 x {0} = {1}", i, 19 * i);
+            }
+            Console.WriteLine("Pressione qualquer tecla para continuar para a próxima tabuada.");
+            Console.ReadKey();
+
+            for (i = 1; i <= 10; i += 1)
+            {
+                Console.WriteLine("20 x {0} = {1}", i, 20 * i);
+            }
+            Console.WriteLine("FIM.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
