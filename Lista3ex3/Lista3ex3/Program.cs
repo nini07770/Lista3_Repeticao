48290cf52fba09f197bf7f40e76022063812f6c7
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-05-24 13:54:06 -0300

    Add files via upload

---
+++ Lista3ex3/Lista3ex3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sexo;
+
+            do
+            {
+                Console.WriteLine("Digite o sexo (M ou F): ");
+                sexo = Console.ReadLine().ToUpper();
+
+                if (sexo != "M" && sexo != "F")
+                {
+                    Console.WriteLine("Sexo inválido. Por favor, digite M ou F.");
+                }
+            } while (sexo != "M" && sexo != "F");
+
+            Console.WriteLine("Sexo aceito:{0} ", sexo);
+        }
+    }
+}
