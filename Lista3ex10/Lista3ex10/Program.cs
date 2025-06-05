d85e2678858c13bfa22f77c05e76de3bf2be5cd3
nini07770 <altino.nicole@aluno.ifsp.edu.br>
2025-06-04 22:05:58 -0300

    Add files via upload

---
+++ Lista3ex10/Lista3ex10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero, soma = 0;
+            double maior = double.MinValue;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Digite um número: ");
+                numero = Double.Parse(Console.ReadLine());
+                soma = soma + numero;
+                
+                if (numero > maior) 
+                {
+                    maior = numero;
+                }
+
+            }
+            Console.WriteLine("Maior número: " + maior);
+            Console.WriteLine("Soma total: " + soma);
+            Console.WriteLine("Média: " + (soma / 10));
+            Console.ReadKey();
+        }
+    }
+}
